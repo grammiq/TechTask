c3a32a48f6f1888a91a44b4e2caed2ab32d676dd
Vasyl <williamme260790@gmail.com>
2023-12-04 19:21:46 +0200

    added task1

---
+++ Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+// Task 1
+    public class Logger
+    {
+        public static void log_message(string logFile, string logEvent, string logType)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string logEntry = $"[{timestamp}] [{logType}] [{logEvent}]";
+            string docPath =
+              Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter writer = new StreamWriter(Path.Combine(docPath, logFile), true))
+            {
+                writer.WriteLine(logEntry);
+            }
+        }
+    }
